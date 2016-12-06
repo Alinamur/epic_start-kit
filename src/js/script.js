75a7c6f739d10cea9938b78d7b3fb3a9f2a9c344
Alinamur <abeille006@gmail.com>
2016-12-06 21:06:11 +0300

    new message

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  // весь ваш код
+  $('#gallery-thumbs').owlCarousel({
+  	items: 4,
+  	loop: true,
+  	center: true,
+  	responsive : {
+    // breakpoint from 480 up
+    680 : {
+        items: 2,
+    },
+    // breakpoint from 768 up
+    800 : {
+        items: 3,
+    }
+},
+  });
 });
